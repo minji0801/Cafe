fc0e811e1e192de59292a63047a7904c769e0d54
Minji Kim <minji_0801@naver.com>
2022-04-12 16:02:41 +0900

    Xcode 프로젝트 생성

---
+++ Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by 김민지 on 2022/04/12.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
